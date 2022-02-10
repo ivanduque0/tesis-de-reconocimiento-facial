ba3c893e57c50ea5963fb77b7c08dd878379f163
ivanduque0 <ivan6a69@gmail.com>
2022-02-09 21:41:07 -0400

    se le agregaron mas cosas al txt dedicado a mi aprendizaje de postgres y se le agregaron las lineas de codigos para verla hora al codigo para enviar datos a las bases de datos

--- enviar_datos.py
+++ enviar_datos.py
@@ -1,10 +1,16 @@
 import psycopg2
+import pytz
+from datetime import datetime
+tz = pytz.timezone('America/Caracas')
+caracas_now = datetime.now(tz)
 
+print(str(caracas_now)[:-22])
 
+conn = None
 try:
     #aqui se establece la coenxion con la base de datos
     conn = psycopg2.connect(
-        database="tesis", user="tesis", password="", host="", port=""
+        database="tesis", user="tesis", password="tesis", host="localhost", port="5432"
     )
 
     #aqui se habilita la opcion para hacer los guardados automaticos a la base de datos
@@ -16,16 +22,15 @@ try:
 
     #aqui se muestra como se hacen las consultas
 
-    cursor.execute('''INSERT INTO EMPLOYEE(FIRST_NAME, LAST_NAME, AGE, SEX,
-    INCOME) VALUES ('Ramya', 'Rama priya', 27, 'F', 9000)''')
-    cursor.execute('''INSERT INTO EMPLOYEE(FIRST_NAME, LAST_NAME, AGE, SEX,
-    INCOME) VALUES ('Vinay', 'Battacharya', 20, 'M', 6000)''')
-    cursor.execute('''INSERT INTO EMPLOYEE(FIRST_NAME, LAST_NAME, AGE, SEX,
-    INCOME) VALUES ('Sharukh', 'Sheik', 25, 'M', 8300)''')
-    cursor.execute('''INSERT INTO EMPLOYEE(FIRST_NAME, LAST_NAME, AGE, SEX,
-    INCOME) VALUES ('Sarmista', 'Sharma', 26, 'F', 10000)''')
-    cursor.execute('''INSERT INTO EMPLOYEE(FIRST_NAME, LAST_NAME, AGE, SEX,
-    INCOME) VALUES ('Tripthi', 'Mishra', 24, 'F', 6000)''')
+    #cursor.execute('''INSERT INTO prueba (valor) VALUES (5);''')
+    # cursor.execute('''INSERT INTO EMPLOYEE(FIRST_NAME, LAST_NAME, AGE, SEX,
+    # INCOME) VALUES ('Vinay', 'Battacharya', 20, 'M', 6000)''')
+    # cursor.execute('''INSERT INTO EMPLOYEE(FIRST_NAME, LAST_NAME, AGE, SEX,
+    # INCOME) VALUES ('Sharukh', 'Sheik', 25, 'M', 8300)''')
+    # cursor.execute('''INSERT INTO EMPLOYEE(FIRST_NAME, LAST_NAME, AGE, SEX,
+    # INCOME) VALUES ('Sarmista', 'Sharma', 26, 'F', 10000)''')
+    # cursor.execute('''INSERT INTO EMPLOYEE(FIRST_NAME, LAST_NAME, AGE, SEX,
+    # INCOME) VALUES ('Tripthi', 'Mishra', 24, 'F', 6000)''')
 
     #con esto se suben las consultas a la base de datos
     conn.commit()
