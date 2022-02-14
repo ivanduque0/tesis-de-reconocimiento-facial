25ab4d6401ed395bc6a0f0a660742f007d02d29d
ivanduque0 <ivan6a69@gmail.com>
2022-02-14 15:22:25 -0400

    se agrego un archivo readme para una explicacion del codigo y un breve tutorial para hacerlo corregit add .

---
+++ rundockerv2_1.sh
@@ -0,0 +1,6 @@
+xhost +local:docker
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+docker run --network=dockernetworktesis --net-alias facerecognition -m 8GB -it --rm -e DISPLAY=$DISPLAY -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH -v ${PWD}:/src -v $(pwd):/app -it c1b77b70f7d1
+xhost -local:docker
