3fb78be7b1fb4d8616c5356a827a10e8eaa2d191
ivanduque0 <ivan6a69@gmail.com>
2022-02-24 14:32:57 -0400

    creacion de nueva rama dedicada a la programacion usando django para el sevidor web

--- dockerfacerecognition/rundockerv2.sh
+++
@@ -1,7 +0,0 @@
-xhost +local:docker
-XSOCK=/tmp/.X11-unix
-XAUTH=/tmp/.docker.xauth
-xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
-docker run -d --network=dockernetworktesis --net-alias postgres -e POSTGRES_PASSWORD=postgres -p 4443:5432 -v $(pwd)/postgresdata:/var/lib/postgresql/data 02da44b1d4a6
-docker run --network=dockernetworktesis --net-alias facerecognition -m 8GB -it --rm -e DISPLAY=$DISPLAY -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH -v ${PWD}:/src -v $(pwd):/app -it c1b77b70f7d1
-xhost -local:docker
--- dockerpinesraspberry/rundockerv2.sh
+++
@@ -1,1 +0,0 @@
-docker run -v $(pwd):/leds --device /dev/gpiomem -it 30d3e8704e40 bash
