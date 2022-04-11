75ac7e28b3d7e77a458b5537475d9860f4514940
ivanduque0 <ivan6a69@gmail.com>
2022-04-11 14:56:12 -0400

    se hizo otra carpeta donde se hicieron los arreglos para que django corriera en conjunto con angular en heroku

---
+++ appheroku(angular)/web/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from .models import contratos, fotos, interacciones, usuarios
+# Register your models here.
+
+class contratosadmin(admin.ModelAdmin):
+
+    fieldsets = [
+        #a la izquierda se pone como se quiere que se llame todo el formulario, y a la derecha se ponen los
+        #nombres de los field en la clase
+        ("agregar contrato", {'fields': ['nombre']}),
+
+    ]
+
+class interaccionesadmin(admin.ModelAdmin):
+
+    fieldsets = [
+        ("Historico de actividad", {'fields': ['cedula','nombre','fecha','hora','razon', 'contrato']}),
+    ]
+
+class usuariosadmin(admin.ModelAdmin):
+
+    fieldsets = [
+        ("usuario", {'fields': ['cedula','nombre','contrato']}),
+    ]
+
+admin.site.register(contratos, contratosadmin)
+admin.site.register(usuarios,usuariosadmin)
+admin.site.register(interacciones, interaccionesadmin)
+admin.site.register(fotos)
---
+++ appheroku(angular)/web/models.py
@@ -0,0 +1,58 @@
+from enum import unique
+from django.db import models  
+# Create your models here.
+
+class contratos(models.Model):
+    #el verbose_name sirve para el nombre que se le dara en el panel de administracion
+    nombre= models.CharField(primary_key=True, max_length=100, verbose_name='nombre del nuevo contrato')
+    class Meta:
+        # con esto se ltera el nombre que se la da a cada clae en el panel de administracion
+        verbose_name_plural = "Contratos"
+    def __str__(self):
+        return self.nombre
+
+class usuarios(models.Model):
+    cedula = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    contrato = models.ForeignKey(contratos, on_delete = models.CASCADE, related_name='contrato', verbose_name='contratousuarios') #models.CharField(max_length=100)
+    
+    class Meta:
+        verbose_name_plural = "Usuarios"
+
+    #def __str__(self):
+    #    return self.cedula
+
+class interacciones(models.Model):
+    cedula = models.ForeignKey(usuarios, db_constraint=False, on_delete = models.DO_NOTHING, related_name='cedulainteracciones') #models.IntegerField()
+    nombre = models.CharField(max_length=50)
+    fecha = models.DateField()
+    hora = models.TimeField()
+    razon = models.CharField(max_length=20)
+    contrato = models.CharField(max_length=100)
+    class Meta:
+        verbose_name_plural = "Interacciones"
+    #def __str__(self):
+        #return self.cedula,self.nombre, self.fecha, self.hora, self.razon
+        #return self.nombre
+
+class horariospermitidos(models.Model):
+
+    class dias(models.TextChoices):
+        LUNES = 'Lunes', 'Lunes'
+        MARTES = 'Martes', 'Martes'
+        MIERCOLES = 'Miercoles','Miercoles'
+        JUEVES = 'Jueves', 'Jueves'
+        VIERNES = 'Viernes', 'Viernes'
+        SABADO = 'Sabado', 'Sabado'
+        DOMINGO = 'Domingo', 'Domingo'
+
+    cedula = models.ForeignKey(usuarios, on_delete=models.CASCADE, related_name="ceduladiaspermitidos")
+    dia=models.CharField(max_length=20, choices=dias.choices, default=dias.LUNES)
+    entrada=models.TimeField()
+    salida=models.TimeField()
+    class Meta:
+        verbose_name_plural = "Horarios"
+
+class fotos(models.Model):
+    cedula = models.ForeignKey(usuarios, on_delete=models.CASCADE, related_name="cedulafoto")
+    foto = models.ImageField(upload_to='personas/')
