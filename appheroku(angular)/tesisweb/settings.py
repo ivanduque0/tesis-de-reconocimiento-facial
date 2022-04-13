4b135bb8c9e17377595052409527ae62b2908709
ivanduque0 <ivan6a69@gmail.com>
2022-04-13 00:14:15 -0400

    se agregaron los archivos estaticos mas recientes. Se encontro una falla con la vista seleccionarcontratoapi, se debe ver que pasa alli

--- appheroku(angular)/tesisweb/settings.py
+++ appheroku(angular)/tesisweb/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'cloudinary',
     'cloudinary_storage',
     'rest_framework',
+    'corsheaders',
     'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -58,6 +59,7 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -66,6 +68,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'tesisweb.urls'
+
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = (
+    'http://tesis-reconocimiento-facial.herokuapp.com/',
+)
 
 TEMPLATES = [
     {
